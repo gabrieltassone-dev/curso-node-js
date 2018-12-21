5b810bff953b0b5b15061f2ff782d4561c9fe139
Gabriel Tassone <gabrieltassone.gt@gmail.com>
2018-12-21 13:33:15 -0300

    Seccion 7

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,6 +1,8 @@
 require('./config/config')
 
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const bodyParser = require('body-parser')
 
@@ -10,37 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('Get usuario')
-})
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function(req, res) {
-
-    let body = req.body
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        })
-    } else {
-        res.json({
-            usuario: body
-        })
-    }
-})
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('Delete usuario')
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err
+    console.log('Base de datos ONLINE');
 })
 
 app.listen(process.env.PORT, () => {
